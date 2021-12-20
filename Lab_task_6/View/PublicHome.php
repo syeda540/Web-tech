1a21b6f03dcb168901ba5f8a9aa2d61c1213925c
subah <syedasubah333@gmail.com>
2021-12-20 22:48:56 +0600

    first commit

---
+++ Lab_task_6/View/PublicHome.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+
+<fieldset>
+	<br>
+		<?php 
+			include 'Head.php';
+		?>
+		<fieldset>
+		<h1>Welcome to sPharmacy</h1>
+		</fieldset>
+	<br>
+	<?php 
+		include 'Foot.php';
+	?>
+</fieldset>
+</body>
+</html> 
---
+++ lab_task_8/View/Account.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+	<link rel="stylesheet" href="../Css/style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+<form>
+<fieldset>
+	
+<h3>Account</h3>
+<table>
+	<tr>
+		<td><a href="Dashboard.php">Dashboard</a></td>
+	</tr>
+
+	<tr>
+		<td><a href="ViewProfile.php">View Profile</a></td>
+	</tr>
+
+	<tr>
+		<td><a href="EditProfile.php">Edit Profile</a></td>
+	</tr>
+
+	<tr>
+		<td><a href="ChangeProfilePicture.php">Change Profile Picture</a></td>
+	</tr>
+
+	<tr>
+		<td><a href="ChangePassword.php">Change Password</a></td>
+	</tr>
+	
+	<tr>
+		<td><a href="../Controller/Logout.php">Logout</a></td>
+	</tr>
+</table>
+
+</fieldset>
+</form>
+</body>
+</html> 
